3ff0ae985e26946743a8c35df57cc4169fbc9266
dgf <github@dagnu.de>
2012-12-18 22:35:11 +0100

    replace search icon

--- src/main/java/com/poemspace/dm4/migrations/Migration3.java
+++ src/main/java/com/poemspace/dm4/migrations/Migration3.java
@@ -1,10 +1,6 @@
 package com.poemspace.dm4.migrations;
 
-import java.util.List;
-
-import com.poemspace.dm4.CriteriaCache;
 import static com.poemspace.dm4.MigrationUtils.*;
-
 import de.deepamehta.core.service.Migration;
 
 public class Migration3 extends Migration {
@@ -16,13 +12,15 @@ public class Migration3 extends Migration {
                 "/com.poemspace.dm4-poemspace/images/birdhouse.png");
         changeIcon(dms, "dm4.contacts.person", "/com.poemspace.dm4-poemspace/images/bird.png");
         changeIcon(dms, "dm4.notes.note", "/com.poemspace.dm4-poemspace/images/pen.png");
-        changeIcon(dms, "dm4.webclient.search", "/com.poemspace.dm4-poemspace/images/bullseye.png");
+        changeIcon(dms, "dm4.webclient.search", "/com.poemspace.dm4-poemspace/images/basket.png");
 
         // add criteria type aggregations
-        List<String> criteriaTypeUris = new CriteriaCache(dms).getTypeUris();
+        String[] criteriaTypeUris = { "dm4.poemspace.art", //
+                "dm4.poemspace.bezirk", //
+                "dm4.poemspace.gattung", //
+                "dm4.poemspace.kiez" };
         addCriteriaAssocDefs(dms, "dm4.poemspace.campaign", criteriaTypeUris);
         addCriteriaAssocDefs(dms, "dm4.contacts.person", criteriaTypeUris);
         addCriteriaAssocDefs(dms, "dm4.contacts.institution", criteriaTypeUris);
     }
-
 }
